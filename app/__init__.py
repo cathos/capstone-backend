30706939380985cb82affdeadbd1668ff10e4ece
cathos <cathos@startmail.com>
2022-08-10 17:18:07 -0700

    remove config mapping

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    # )
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
